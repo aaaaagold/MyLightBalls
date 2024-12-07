20ced923832cac5f73049970a4e8a840eb390ba9
agold404 <10041465+aaaaagold@users.noreply.github.com>
2024-12-07 19:03:36 +0800

    update plugins

--- docs/js/plugins/agold404_NameField.js
+++ docs/js/plugins/agold404_NameField.js
@@ -4,8 +4,8 @@
  * @author agold404
  * 
  * @help showing name field near by message window
- * \NAMEFIELD_INIT:" name "
- * \NAMEFIELD_SHOW:" name "
+ * \NAMEFIELD:" name "
+ * name drawn is evaluated LIKE: eval('" name "')
  * 
  * 
  * This plugin can be renamed as you want.
@@ -18,26 +18,59 @@ const params=PluginManager.parameters(pluginName);
 t=[
 undefined,
 params, // 1: plugin params
-/\\NAMEFIELD_(INIT|SHOW):("((\\\\)*\\"|[^"\\]|\\[^"])*")/g,
+/(?<![\\])(\\\\)*(\\NAMEFIELD:("((\\\\)*\\"|[^"\\]|\\[^"])*"))/, // 2: pattern
 ];
 
-new cfc(Game_Message.prototype).add('add',function f(){
+new cfc(Game_Interpreter.prototype).add('command101_text',function f(){
+	const texts=$gameMessage._texts;
+	const strt=texts.length;
 	const rtv=f.ori.apply(this,arguments);
-	this.startupEval_start();
+	$gameMessage._nameField=undefined;
+	for(let x=0,xs=rtv.length;x!==xs;++x){
+		const src=rtv[x];
+		const m=src.match(f.tbl[2]); if(!m) continue;
+		let res="";
+		res+=src.slice(0,m.index+(m[1]?m[1].length:0));
+		res+=src.slice(m.index+m[0].length);
+		rtv[x]=texts[strt+x]=res;
+		$gameMessage._nameField=EVAL.call(this,m[3]);
+		break;
+	}
 	return rtv;
-}).add('terminate_after',function f(){
-	this.startupEval_terminate();
-	return f.ori.apply(this,arguments);
-}).addBase('startupEval_start',function f(){
-	const s=f.tbl[1]._onbootStart;
-	{ let k,r,t,f; { return eval(s); } }
-},t).addBase('startupEval_terminate',function f(){
-	const s=f.tbl[1]._onbootTerminate;
-	{ let k,r,t,f; { return eval(s); } }
 },t);
 
-{ const s=params._onload;
-{ let k,r,t,f; { return eval(s); } }
-}
+new cfc(Window_Message.prototype).add('startMessage',function f(){
+	if($gameMessage._nameField!=null){
+		let w=this.textWidth($gameMessage._nameField);
+		if(w<Window_Base._faceWidth) w=Window_Base._faceWidth;
+		const pad=((this.standardPadding()+this.textPadding())<<1)+1;
+		w+=pad;
+		if(this.width<w) w=this.width;
+		if(!this._nameField){
+			this._nameField=new Window_Help(1);
+			this._nameField.y=-this._nameField.height;
+			this._nameField.openness=0;
+		}
+		this._nameField.width=w;
+		this._nameField.contents.clear();
+		this._nameField.drawText($gameMessage._nameField,this.textPadding(),0,w-pad,'center');
+		this._nameField.enabled=1;
+		this.addChild(this._nameField);
+	}else if(this._nameField) this._nameField.enabled=0;
+	return f.ori.apply(this,arguments);
+}).add('update',function f(){
+	const rtv=f.ori.apply(this,arguments);
+	this.update_nameField();
+	return rtv;
+}).add('update_nameField',function f(){
+	if(this._nameField){
+		//this._nameField.openness=this.openness;
+		if(!this._nameField.enabled||this.isClosing()||this.isClosed()) this._nameField.close();
+		else if(this.isOpening()||this.isOpen()) this._nameField.open();
+	}
+}).add('onclosed',function f(){
+	if(this._nameField) this._nameField.openness=this._nameField.enabled=0;
+	return f.ori.apply(this,arguments);
+});
 
 })();
